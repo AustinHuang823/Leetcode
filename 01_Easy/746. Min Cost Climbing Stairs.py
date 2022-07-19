c8a0b8a96f2da6b9fe9ed983628a08661290501f
AustinHuang823 <87944244+AustinHuang823@users.noreply.github.com>
2022-07-19 16:32:09 +0800

    Add files via upload

---
+++ 01_Easy/746. Min Cost Climbing Stairs.py
@@ -0,0 +1,42 @@
+from typing import List
+import timeit
+
+start = timeit.default_timer()
+
+class Solution2:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        n = len(cost)
+        dp = {}
+        
+        def dfs(i):
+            if i >= n:
+                return 0
+            if i in dp:
+                return dp[i]
+            
+            dp[i] = min(dfs(i+1) + cost[i], dfs(i+2) + cost[i])
+            return dp[i]
+        
+        stop = timeit.default_timer()
+        print('Passed Solution time: ', stop - start)  
+        
+        return min(dfs(0), dfs(1))
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        def dfs(i):
+            if i < len(cost):
+                return min(dfs(i+1) + cost[i], dfs(i+2) + cost[i])
+            if i >= len(cost):
+                return 0
+
+        stop = timeit.default_timer()
+        print('My time limt exceeded solution time: ', stop - start)
+        
+        return min(dfs(0), dfs(1))
+            
+        
+if __name__ == '__main__': #vscode main line
+    sol = Solution2()
+    cost = [841,462,566,398,243,248,238,650,989,576,361,126,334,729,446,897,456,487,145,156,123]
+    print(sol.minCostClimbingStairs(cost))

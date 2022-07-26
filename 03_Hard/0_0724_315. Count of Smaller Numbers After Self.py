dcf0cf6d1e409afe26da9a50a2493ff434bb1968
AustinHuang823 <87944244+AustinHuang823@users.noreply.github.com>
2022-07-26 17:56:36 +0800

    Add files via upload

---
+++ 03_Hard/0_0724_315. Count of Smaller Numbers After Self.py
@@ -0,0 +1,22 @@
+from typing import List
+import collections
+
+class Solution:
+    def countSmaller(self, nums: List[int]) -> List[int]:
+        cnt= collections.Counter(nums)
+        counts = []
+        for i in range(len(nums)):
+            if nums[i] in cnt:
+                cnt[nums[i]] -= 1
+                y = sorted(cnt.items(), key=lambda x:(-x[0]))
+                
+                # I think below is the line need to fix
+                counts.append(sum([j[1] for j in y]))
+                print("here for nums[i] ==",nums[i],",I want to sum y[1] right to y[0] ==", nums[i],"in", y)
+                
+        return counts
+        
+if __name__ == '__main__':
+    nums = [5,2,6,1]
+    sol = Solution()
+    print("ans ==", sol.countSmaller(nums),"which is wrong, for this specific case, ans should be [2,1,1,0]")

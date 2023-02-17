fe24b94fc1fc5a61fdd6f1d2950202c1823be3a3
AustinHuang823 <87944244+AustinHuang823@users.noreply.github.com>
2023-02-17 13:29:17 -0500

    Add files via upload

---
+++ 02_Medium/143. Reorder List.py
@@ -0,0 +1,63 @@
+import DS
+from typing import Optional
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        # dummy = ListNode(0)
+        #find middle
+        slow = fast = head
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        # reverse
+        prev, curr = None, slow.next
+        while curr:
+            nextt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nextt
+        slow.next = None
+
+        #merge
+        head1, head2 = head, prev
+        while head2:
+            curnext = head1.next
+            head1.next = head2
+            head1 = head2
+            head2 = curnext
+
+class SolutionTLE:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        def findlast(root):
+            prev = ListNode(0)
+            prev.next = root
+            while root:
+                prev, root = prev.next, root.next
+            return prev
+        
+        dummy = ListNode(0)
+        dummy.next = head
+        while head:
+            if not head.next or not head.next.next: break
+            cur_next = head.next
+            head.next = findlast(head)
+            head.next.next = cur_next
+            head = head.next.next
+        
+        return dummy.next
+
+if __name__ == '__main__':
+    head = DS.arr2LinkedNode([1,2,3,4,5])
+    sol = Solution()
+    print(DS.print_ListNode(sol.reorderList(head)))
